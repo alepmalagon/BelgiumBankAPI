711a95d9f5cc78039a6730a9462ca92762031963
gsi-malagon <malagon@generalsoftwareinc.com>
2022-12-26 15:03:18 -0500

    fix: naming issues in Models

--- BelgiumBankAPI/Data/ApiContext.cs
+++ BelgiumBankAPI/Data/ApiContext.cs
@@ -6,6 +6,8 @@ namespace BelgiumBankAPI.Data
     public class ApiContext: DbContext
     {
         public DbSet<BankAccount> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Customer> Customers{ get; set; }
         public ApiContext(DbContextOptions<ApiContext> options)
             : base(options) { }
     }
