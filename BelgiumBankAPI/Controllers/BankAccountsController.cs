ba25c18bd6b0a66f45107829823a26848a9d7db0
gsi-malagon <malagon@generalsoftwareinc.com>
2022-12-26 13:18:57 -0500

    feat: Adding controller

---
+++ BelgiumBankAPI/Controllers/BankAccountsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BelgiumBankAPI.Models;
+using BelgiumBankAPI.Data;
+
+namespace BelgiumBankAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAccountsController : ControllerBase
+    {
+        private readonly ApiContext apiContext;
+
+        public BankAccountsController(ApiContext context)
+        { 
+            apiContext = context;
+        }
+    }
+}
