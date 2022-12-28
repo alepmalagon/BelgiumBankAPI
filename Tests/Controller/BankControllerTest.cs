bcae102e2bb7f6833641feeac998af125e360943
gsi-malagon <malagon@generalsoftwareinc.com>
2022-12-28 10:17:30 -0500

    feat: Adding controller testing

---
+++ Tests/Controller/BankControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using FakeItEasy;
+using BelgiumBankAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Controller
+{
+    public class BankControllerTest
+    {
+        public BankControllerTest() 
+        {
+            apiContext = A.Fake<ApiContext>(); 
+        }
+    }
+}
